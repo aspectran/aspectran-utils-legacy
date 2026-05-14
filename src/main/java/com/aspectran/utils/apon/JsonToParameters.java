f95f20dd70f027018da84bd25a3d045e9b5d6304
juho.jeong <aspectran@gmail.com>
2026-05-14 10:55:01 +0900

    Add a lenient mode constructor to JsonToParameters
    Introduced a new constructor allowing lenient parsing to be explicitly enabled or disabled. Updated the static `from` method to utilize this new constructor for improved flexibility and clarity.

--- src/main/java/com/aspectran/utils/apon/JsonToParameters.java
+++ src/main/java/com/aspectran/utils/apon/JsonToParameters.java
@@ -63,6 +63,14 @@ public class JsonToParameters {
     }
 
     /**
+     * Create a converter that produces a default {@link VariableParameters} container.
+     * @param lenient {@code true} to enable lenient parsing, {@code false} for strict parsing
+     */
+    public JsonToParameters(boolean lenient) {
+        this(null, lenient);
+    }
+
+    /**
      * Create a converter that will instantiate the given {@code requiredType}
      * for the target {@link Parameters} container.
      * @param requiredType the concrete Parameters implementation to instantiate
@@ -303,7 +311,7 @@ public class JsonToParameters {
      */
     @NonNull
     public static Parameters from(String json, boolean lenient) throws IOException {
-        return new JsonToParameters(null, lenient).read(json);
+        return new JsonToParameters(lenient).read(json);
     }
 
     /**
